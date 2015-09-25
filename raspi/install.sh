685b010a6a776d205b318972ff129bf9e5c25f6c
Ashray Jain <ashrayj11@gmail.com>
2015-09-25 23:51:22 +0800

    add uart service to auto start on boot up

--- raspi/install.sh
+++ raspi/install.sh
@@ -2,5 +2,6 @@ echo "Installing..."
 sudo service uart stop
 sudo cp uart.sh /etc/init.d/uart.sh
 sudo chmod +x /etc/init.d/uart.sh
+sudo update-rc.d uart default
 sudo service uart start
 echo "Done"
