c5e32fc2df37a513e8dc74818d9f9852dd253df7
Ashray Jain <ashrayj11@gmail.com>
2015-09-25 17:39:18 +0800

    added directions

--- raspi/uart.py
+++ raspi/uart.py
@@ -1,16 +1,50 @@
+#!/usr/bin/env python
 import logging
+import logging.handlers
+import argparse
+import sys
+import time
 import serial
 
-LOG_FILENAME = '/home/pi/logs/uart.log'
-logging.basicConfig(filename=LOG_FILENAME,
-                    level=logging.INFO)
+LOG_FILENAME = 'uart.log'
+LOG_LEVEL = logging.INFO
+
+parser = argparse.ArgumentParser(description="Uart service")
+parser.add_argument("-l", "--log",
+                    help="log file (default: " + LOG_FILENAME + ")")
+args = parser.parse_args()
+if args.log:
+        LOG_FILENAME = args.log
+
+logger = logging.getLogger(__name__)
+logger.setLevel(LOG_LEVEL)
+handler = logging.handlers.TimedRotatingFileHandler(
+    LOG_FILENAME, when='midnight', backupCount=3)
+handler.setFormatter(logging.Formatter(
+    '%(ascitime)s %(levelname)-8s %(message)s'))
+logger.addHandler(handler)
+
+
+class UartLogger(object):
+
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+
+    def write(self, message):
+        if message.rstrip() != '':
+            self.logger.log(self.level, message.rstrip())
+
+sys.stdout = UartLogger(logger, logging.INFO)
+sys.stderr = UartLogger(logger, logging.ERROR)
 
 
 class UartHandler():
 
-    def __init__(self):
-        self.ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=10)
-        self.logger = logging
+    def __init__(self, logger):
+        print "FSGDFG"
+        self.ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+        self.logger = logger
 
         self.logger.info('Opening serial line')
         self.ser.open()
@@ -69,5 +103,5 @@ class UartHandler():
         pass
 
 
-if __name__ == '__main__':
-    uart = UartHandler()
+#uart = UartHandler(logger)
+
