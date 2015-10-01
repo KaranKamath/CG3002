a37fee737e687ccb5656eaab02dd6417b1330f08
Ashray Jain <ashrayj11@gmail.com>
2015-10-01 12:52:08 +0800

    fix altimeter and heading gen

--- raspi/heading_generator.py
+++ raspi/heading_generator.py
@@ -1,7 +1,8 @@
 from math import sqrt, atan2, pi
+import sys
 
-m_min = (2468, -417, -1658)
-m_max = (2581, -277, -1574)
+m_min = [32767, 32767, 32767]
+m_max = [-32768, -32768, -32768]
 
 
 def vector_dot(a, b):
@@ -33,12 +34,35 @@ def heading(a, m, f):
 
     heading = atan2(vector_dot(e, f), vector_dot(n, f)) * 180 / pi
     return (heading + 360) if heading < 0 else heading
-    
+
+
+def calibrate(db):
+    global m_min, m_max
+    counter = 1000
+    current = int(time.time()*1000)
+    while counter > 0:
+        counter -= 1
+        data = db.fetch(since=current, sid=1)
+        if data:
+            current = data[-1][0]
+            data = data[-1][2]
+            for i in range(3):
+                if data[i + 3] < m_min[i]:
+                    m_min[i] = data[i + 3]
+                if data[i + 3] > m_max[i]:
+                    m_max[i] = data[i + 3]
+
 if __name__ == '__main__':
     import time
     from Database.db import DB
     foo = DB()
-    current = int(round(time.time() * 1000))
-    foo.fetch()
-    
-
+    # calibrate(foo)
+    current = int(time.time()*1000)
+    while True:
+        data = foo.fetch(since=current, sid=1)
+        if data:
+            data = data[-1]
+            heading_v = heading(data[2][:3], data[2][3:], [0, 0, 1])
+            sys.stdout.flush()
+            sys.stdout.write("Heading: %s deg   \r" % (heading_v,))
+            current = data[0]
