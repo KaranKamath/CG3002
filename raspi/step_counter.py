9f5fbb05f69bc0f6cda48a6067024dc4b5b5a3d0
Karan Kamath <karankamath26@gmail.com>
2015-10-15 14:41:00 +0800

    Step Counter V0.2

--- raspi/step_counter.py
+++ raspi/step_counter.py
@@ -3,8 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from enum import Enum
 from db import DB
+from scipy import signal
 
-THRESHOLD = 6100
+MIN_THRESHOLD = 0.2
+FILTER_ORDER = 5
+FS = 10 #Sample Rate
+CUTOFF = 1
+ACC_MAX_VAL = 32768
 
 plt.axis([0, 1000, 0, 1])
 plt.ion()
@@ -12,22 +17,19 @@ plt.show()
 count = 0
 
 
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+  
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = signal.lfilter(b, a, data)
+    return y
+
 def now():
     return int(round(time.time() * 1000))
-
-
-class StepState(Enum):
-    undefined = 0
-    up = 1
-    down = 2
-
-
-def get_state_for_num(num):
-    if num > THRESHOLD:
-        return StepState.up
-    else:
-        return StepState.down
-
 
 class StepCounter(object):
 
@@ -35,37 +37,9 @@ class StepCounter(object):
         self.last_ts = l_ts
         self.count = 0
         self.db = DB()
-        self.step_state = StepState.undefined
 
     def get_count(self):
         return self.count
-
-    def calculate_steps(self, step_states):
-
-        if len(step_states) == 1:
-            if self.step_states == StepState.up and \
-                    step_states[0] == StepState.down:
-                return 1
-            else:
-                return 0
-
-        if self.step_state != StepState.undefined:
-            step_states.insert(0, self.step_state)
-
-        count = 0
-        for i in range(len(step_states) - 1):
-            if step_states[i] == StepState.up and \
-                    step_states[i + 1] == StepState.down:
-                count = count + 1
-
-        return count
-
-    def get_step_states(self, average_vals):
-        return map(lambda x: get_state_for_num(x), average_vals)
- 
-    def movingaverage(self, interval, window_size):
-        window = np.ones(int(window_size))/float(window_size)
-        return np.convolve(interval, window)
 
     def update_count(self):
         fetched_data = sorted(self.db.fetch(sid=1, since=self.last_ts), key=lambda d: d[0])
@@ -77,28 +51,20 @@ class StepCounter(object):
             print "no value"
             return
 
-        for i in fetched_values:
-            plt.scatter(i, count)
-            plt.draw()
-            count += 1
-            time.sleep(0.05)
-
-        fetched_values = self.movingaverage(fetched_values, 3)
-        fetched_values = fetched_values[2:-2]
-        print fetched_values
+        normalized_vals = [ v * 1.0 / ACC_MAX_VAL for v in fetched_values]
+        low_passed_vals = butter_lowpass_filter(normalized_vals, CUTOFF, FS, FILTER_ORDER)
+        peak_indices = signal.argrelmax(low_passed_vals)[0]
+        peak_vals = [low_passed_vals[x] for x in peak_indices]
+        accepted_peaks = [x for x in peak_vals if x > MIN_THRESHOLD]
 
         self.last_ts = fetched_data[-1][0]
 
-        step_states = self.get_step_states(fetched_values)
-
-        self.count = self.count + self.calculate_steps(step_states)
-
-        self.step_state = step_states[-1]
+        self.count = self.count + len(accepted_peaks)
 
 if __name__ == "__main__":
     sc = StepCounter(l_ts=0)
     while True:
         sc.update_count()
         print 'Steps (Cumulative): ', sc.get_count()
-        print 'Current State (0, 1, 2): ', sc.step_state
+        #print 'Current State (0, 1, 2): ', sc.step_state
         time.sleep(5)
