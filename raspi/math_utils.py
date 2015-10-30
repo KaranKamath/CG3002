7bf67bccd5afbad5ab1934ece490511f785851a5
Karan Kamath <karankamath26@gmail.com>
2015-10-30 21:11:19 +0800

    Add dummy turn detection and a math utils file

---
+++ raspi/math_utils.py
@@ -0,0 +1,6 @@
+import math
+
+def euclidean_distance(p1, p2):
+    return ((p2[1] - p1[1]) ** 2) + ((p2[0] - p1[0]) ** 2)
+
+
--- raspi/step_counter.py
+++ raspi/step_counter.py
@@ -7,6 +7,7 @@ STEP_LENGTH = 36
 class StepCounter(object):
 
     STEP_THRESHOLD = 5000
+    TURN_THRESHOLD = 1000
 
     def __init__(self, x, y, logger):
         self.x = x
@@ -15,6 +16,15 @@ class StepCounter(object):
         self.state = 0
         self.step_count = 0
         self.prev_val = 0
+        self.turn_history = []
+
+    def did_turn(self, gyroX):
+        if abs(gyroX) > TURN_THRESHOLD:
+            self.log.info("Turn Detected")
+            self.turn_history.append((self.x, self.y))
+
+        if len(self.turn_history) > 5:
+            self.turn_history = self.turn_history[-5:]
 
     def update_coords(self, data, heading):
         if data[-2] > self.STEP_THRESHOLD:
