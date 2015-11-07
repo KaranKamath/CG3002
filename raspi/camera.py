fd4b25bc50363f9c0ef219f4b221e2842e1a7d72
Ashray Jain <ashrayj11@gmail.com>
2015-11-07 16:08:24 +0800

    add image deployment stuff

--- raspi/camera.py
+++ raspi/camera.py
@@ -15,7 +15,7 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
     camera.start_preview()
     time.sleep(1)
-    for i, filename in enumerate(camera.capture_continuous('image{counter:02d}.jpg')):
+    for i, filename in enumerate(camera.capture_continuous('images/image{counter:02d}.jpg')):
         print('Captured image %s' % filename)
         if i == 100:
             break
