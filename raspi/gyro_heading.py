a2d85d7699c258fabc9009b63d64d947a360a197
Karan Kamath <karankamath26@gmail.com>
2015-11-07 22:10:54 +0800

    Change vector (heading gyro)

--- raspi/gyro_heading.py
+++ raspi/gyro_heading.py
@@ -5,9 +5,9 @@ GYRO_SENSITIVITY = 65.536
 dt = 0.1
 
 def get_new_heading(angle, accData, gyroData):
-    angle += gyroData[2] * dt / GYRO_SENSITIVITY
+    angle += gyroData[0] * dt / GYRO_SENSITIVITY
     forceMagnitudeApprox = sum([abs(x) for x in accData])
     if ACC_SENSITIVITY < forceMagnitudeApprox < (4 * ACC_SENSITIVITY):
-        heading_acc = atan2(accData[1], accData[0]) * 180.0 / pi
+        heading_acc = atan2(accData[1], accData[2]) * 180.0 / pi
         angle = 0.98 * angle + 0.02 * heading_acc
     return angle
