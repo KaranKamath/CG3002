b33a6874199b2f4e9196ba36d50bb97f385eb3d3
Karan Kamath <karankamath26@gmail.com>
2015-11-07 18:25:06 +0800

    gyro heading calc added

---
+++ raspi/gyro_heading.py
@@ -0,0 +1,17 @@
+from math import atan2, pi
+ACC_SENSITIVITY = 32768.0 / 2
+GYRO_SENSITIVITY = 32768 / 245
+dt = 0.1
+
+def get_new_heading(angle, accData, gyroData):
+
+    angle += gyroData[2] * dt / GYRO_SENSITIVITY
+
+    forceMagnitudeApprox = sum([abs(x) for x in accData])
+
+    if ACC_SENSITIVITY < forceMagnitudeApprox < (4 * ACC_SENSITIVITY):
+        heading_acc = atan2(accData[0], accData[1]) * 180.0 / pi
+
+        angle = 0.98 * angle + 0.02 * heading_acc
+
+    return angle 
