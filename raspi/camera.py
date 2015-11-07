1d639d5524e55e59212e82f5fb8273b6504a9ec5
Ashray Jain <ashrayj11@gmail.com>
2015-11-07 16:29:45 +0800

    Revert "Grayscale"
    This reverts commit e4a9f186eb9f2288f094b0cc2b5378a23dc127e5.

--- raspi/camera.py
+++ raspi/camera.py
@@ -1,15 +1,5 @@
 import time
 import picamera
-import cv2
-
-
-def process(i):
-    id = str(i)
-    if int(i) < 10:
-        id = '0' + str(id)
-
-    img = cv2.imread('image'+id+'.jpg', 0)
-    cv2.imwrite('grayscale' + id, img)
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
@@ -19,7 +9,5 @@ with picamera.PiCamera() as camera:
         print('Captured image %s' % filename)
         if i == 100:
             break
-
-        process(i)
         time.sleep(60)
     camera.stop_preview()
