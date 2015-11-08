aca92b9a36e660147f026ddd3d9c9b6e29c9e19e
Karan Kamath <karankamath26@gmail.com>
2015-11-08 21:39:23 +0800

    Update camera script to use cv2

--- raspi/camera.py
+++ raspi/camera.py
@@ -1,13 +1,36 @@
 import time
 import picamera
+import cv2
+
+DIR_PATH = 'images/'
+RES_HEIGHT = 720
+RES_WIDTH = 1280
+counter = 1
+
+def process_img(img):
+    global counter
+
+    crop_img = img[(RES_HEIGHT / 2):, (RES_WIDTH / 4):(3 * RES_WIDTH / 4)] 
+    blur = cv2.blur(crop_img, (5,5))                                 
+    canny = cv2.Canny(blur, 33, 100)
+
+    cv2.imwrite(DIR + counter + '-blurred.jpg', blur)                               
+    cv2.imwrite(DIR + counter + '-original.jpg', img)                                 
+    cv2.imwrite(DIR + counter + '-canny.jpg', canny)   
+
+    counter += 1
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1280, 720)
-    camera.start_preview()
-    time.sleep(1)
-    for i, filename in enumerate(camera.capture_continuous('images/image{counter:02d}.jpg')):
-        print('Captured image %s' % filename)
-        if i == 1000:
-            break
-        time.sleep(30)
-    camera.stop_preview()
+    camera.resolution = (RES_WIDTH, RES_HEIGHT)
+    rawCapture = PiRGBArray(camera)
+ 
+    # allow the camera to warmup
+    time.sleep(0.1)
+
+    while True:
+        # grab an image from the camera
+        camera.capture(rawCapture, format="bgr")
+        img = rawCapture.array
+        process_img(img)
+        time.sleep(1)
+
