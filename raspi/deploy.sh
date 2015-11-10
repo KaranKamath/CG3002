98e6b70c440c590f03fb1f18c23a807cb6a9aa60
Karan Kamath <karankamath26@gmail.com>
2015-11-10 15:55:27 +0800

    Make deploy script less YOLO

--- raspi/deploy.sh
+++ raspi/deploy.sh
@@ -1,10 +1,11 @@
 echo "Deploying..."
-# scp pi@$1:/home/pi/db/uart.db backups/uart.$(date '+%I-%M-%S').db
-# TIMESTAMP=$(date '+%s')
-mkdir ../images/$TIMESTAMP
-scp -r 'pi@192.168.43.77:/home/pi/cg3002/images/*' ../images/$TIMESTAMP
-rsync -r --delete ../raspi/ pi@$1:~/cg3002/
-echo "Done."
+scp pi@$1:/home/pi/db/uart.db backups/uart.$(date '+%I-%M-%S').db
+TIMESTAMP=$(date '+%s')
+echo $TIMESTAMP
+mkdir -p ../images/$TIMESTAMP && \
+scp -r 'pi@192.168.43.77:/home/pi/cg3002/images/*' ../images/$TIMESTAMP && \
+rsync -r --delete ../raspi/ pi@$1:~/cg3002/ && \
+echo "Done." || echo "Failed"
 
 COMMAND="cd ~/cg3002;"
 if [ "$2" == "--install-prereqs" ]; then
