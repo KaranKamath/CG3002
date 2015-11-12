13098305cdf55a6150206bc4ecd6d58bf7e9db04
Karan Kamath <karankamath26@gmail.com>
2015-11-12 14:42:17 +0800

    Add proto fast cam script

---
+++ raspi/fast_cam.py
@@ -0,0 +1,77 @@
+import io
+import time
+import threading
+import picamera
+import uuid
+import cv2
+
+# Create a pool of image processors
+done = False
+lock = threading.Lock()
+pool = []
+DIR_PATH = 'images/'
+COUNTER = 0
+
+class ImageProcessor(threading.Thread):
+    def __init__(self):
+        super(ImageProcessor, self).__init__()
+        self.stream = io.BytesIO()
+        self.event = threading.Event()
+        self.terminated = False
+        self.start()
+
+    def run(self):
+        # This method runs in a separate thread
+        global done
+        while not self.terminated:
+            # Wait for an image to be written to the stream
+            if self.event.wait(1):
+                try:
+                    global COUNTER
+                    self.stream.seek(0)
+                    img = cv2.imread(self.stream)
+                    cv2.imwrite(DIR_PATH + 'image-' + str(uuid.uuid4()) + '.jpg', img)
+                    # Read the image and do some processing on it
+                    #Image.open(self.stream)
+                    #...
+                    #...
+                    # Set done to True if you want the script to terminate
+                    # at some point
+                    #done=True
+                finally:
+                    # Reset the stream and event
+                    self.stream.seek(0)
+                    self.stream.truncate()
+                    self.event.clear()
+                    # Return ourselves to the pool
+                    with lock:
+                        pool.append(self)
+
+def streams():
+    while not done:
+        with lock:
+            if pool:
+                processor = pool.pop()
+            else:
+                processor = None
+        if processor:
+            yield processor.stream
+            processor.event.set()
+        else:
+            # When the pool is starved, wait a while for it to refill
+            time.sleep(0.1)
+
+with picamera.PiCamera() as camera:
+    pool = [ImageProcessor() for i in range(4)]
+    camera.resolution = (640, 480)
+    camera.framerate = 30
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture_sequence(streams(), use_video_port=True)
+
+# Shut down the processors in an orderly fashion
+while pool:
+    with lock:
+        processor = pool.pop()
+    processor.terminated = True
+    processor.join()
